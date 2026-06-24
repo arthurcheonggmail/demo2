ffc26c97ad0ec1c8e9b9a9fd0795332b8c0e8c8a
arthurcheonggmail <arthurcheong@gmail.com>
2026-06-24 18:17:08 +0800

    [feat] Add one more line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Add Hello World!");
+        System.out.println("One more line!");
     }
 }
