1d0c4fb6c2465f228b2a807a4b60ed1276286745
arthurcheonggmail <arthurcheong@gmail.com>
2026-06-24 11:59:47 +0800

    [feat] Add new test line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -6,5 +6,6 @@ package com.sddevops.maven.eclipse;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Add Hello World!");
     }
 }
